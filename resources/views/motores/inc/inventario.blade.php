5781cf52ad9adef0bf325b35f486941ca03606ab
Sam <samuel.mayorga@cmeamir.com>
2021-07-16 18:09:37 -0600

    inventario terminado

--- resources/views/motores/inc/inventario.blade.php
+++ resources/views/motores/inc/inventario.blade.php
@@ -9,8 +9,8 @@
     </div> <!--x title -->
     <div class="x_content">
         <div class="form-group row">
-            <label for="acoplelbl" class="control-label col-md-4 col-sm-4 col-xs-12">Acople o polea:</label>
-            <div class="col-md-8 col-sm-8 col-xs-12">  
+            <label for="acoplelbl" class="control-label col-md-3 col-sm-3 col-xs-12">Acople o polea:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
 `                <div>
                          <input type="radio" class="flat"  name="iCheckAcopple"  value="1"> Si (buen estado)
                   </div>
@@ -22,10 +22,10 @@
                   </div>
             </div><!--columna -->
         </div><!--form group -->
-        <div class="clearfix"></div>
+        
         <div class="form-group row">
-            <label for="cajalbl" class="control-label col-md-4 col-sm-4 col-xs-12">Caja de Conexiones:</label>
-            <div class="col-md-8 col-sm-8 col-xs-12">  
+            <label for="cajalbl" class="control-label col-md-3 col-sm-3 col-xs-12">Caja de Conexiones:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
                 <div>
                         <input type="radio" class="flat"  name="iCheckCaja"  value="1"> Si (buen estado)
                 </div>
@@ -35,6 +35,154 @@
                 <div>
                         <input type="radio" class="flat" checked="" name="iCheckCaja"  value="3"> No
                 </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">            
+            <label for="tapa_conexionlbl" class="control-label col-md-3 col-sm-3 col-xs-12">Tapa Caja de Conexiones:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckTapa"   value="1"> Si (buen estado)
+                  </div>
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckTapa"   value="2"> Si (Mal estado)
+                  </div> 
+                  <div>
+                         <input type="radio" class="flat"  checked="" name="iCheckTapa"   value="3"> No
+                  </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">
+            <label for="difusorlbl" class="control-label col-md-3 col-sm-3 col-xs-12">Difusor:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckDifusor"   value="1"> Si (buen estado)
+                  </div>
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckDifusor"   value="2"> Si (Mal estado)
+                  </div> 
+                  <div>
+                         <input type="radio" class="flat"  checked="" name="iCheckDifusor"   value="3"> No
+                  </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">
+            <label for="ventiladorlbl" class="control-label col-md-3 col-sm-3 col-xs-12">Ventilador:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckVentilador"   value="1"> Si (buen estado)
+                  </div>
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckVentilador"   value="2"> Si (Mal estado)
+                  </div> 
+                  <div>
+                         <input type="radio" class="flat"  checked="" name="iCheckVentilador"  value="3"> No
+                  </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">
+            <label for="borneralbl" class="control-label col-md-3 col-sm-3 col-xs-12">Bornera:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckBornera"   value="1"> Si (buen estado)
+                  </div>
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckBornera"   value="2"> Si (Mal estado)
+                  </div> 
+                  <div>
+                         <input type="radio" class="flat"  checked="" name="iCheckBornera"   value="3"> No
+                  </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">
+            <label for="cunialbl" class="control-label col-md-3 col-sm-3 col-xs-12">Cu&ntilde;a:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckCuna"   value="1"> Si (buen estado)
+                  </div>
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckCuna"   value="2"> Si (Mal estado)
+                  </div> 
+                  <div>
+                         <input type="radio" class="flat"  checked="" name="iCheckCuna"   value="3"> No
+                  </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">
+            <label for="graseraslbl" class="control-label col-md-3 col-sm-3 col-xs-12">Graseras:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                <div>
+                    <input type="radio" class="flat"  name="iCheckGrasera"   value="1"> Si (buen estado)
+                </div>
+                <div>
+                    <input type="radio" class="flat"  name="iCheckGrasera"   value="2"> Si (Mal estado)
+                </div> 
+                <div>
+                    <input type="radio" class="flat" checked="" name="iCheckGrasera"   value="3"> No
+                </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">
+            <label for="cancamolbl" class="control-label col-md-3 col-sm-3 col-xs-12">C&aacute;ncamo:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckCancamo"   value="1"> Si (buen estado)
+                  </div>
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckCancamo"   value="2"> Si (Mal estado)
+                  </div> 
+                  <div>
+                         <input type="radio" class="flat"  checked="" name="iCheckCancamo"   value="3"> No
+                  </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">
+        <label for="placalbl" class="control-label col-md-3 col-sm-3 col-xs-12">Placa de Datos:</label>
+        <div class="col-md-9 col-sm-9 col-xs-12">  
+            <div>
+                   <input type="radio" class="flat"  name="iCheckPlaca"   value="1"> Si (buen estado)
+            </div>
+            <div>
+                   <input type="radio" class="flat"  name="iCheckPlaca"   value="2"> Si (Mal estado)
+            </div> 
+            <div>
+                   <input type="radio" class="flat"  checked="" name="iCheckPlaca"   value="3"> No
+            </div>
+        </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">
+            <label for="tornilloslbl" class="control-label col-md-3 col-sm-3 col-xs-12">Tornillos Completos:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckTornillos"   value="1"> Si (buen estado)
+                  </div>
+                  <div>
+                         <input type="radio" class="flat"  name="iCheckTornillos"   value="2"> Si (Mal estado)
+                  </div> 
+                  <div>
+                         <input type="radio" class="flat"  checked="" name="iCheckTornillos"   value="3"> No
+                  </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group row">      
+            <label for="capacitoreslbl" class="control-label col-md-3 col-sm-3 col-xs-12">Capacitores:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                <div>
+                        <input type="radio" class="flat"  name="iCheckCapacitores"   value="1"> Si (buen estado)
+                </div>
+                <div>
+                        <input type="radio" class="flat"  name="iCheckCapacitores"   value="2"> Si (Mal estado)
+                </div> 
+                <div>
+                        <input type="radio" class="flat" checked="" name="iCheckCapacitores"   value="3"> No
+                </div>
+            </div><!--columna -->
+        </div><!--form group -->
+        <div class="form-group">
+            
+            <label for="commentInvlbl" class="control-label col-md-3 col-sm-3 col-xs-12">Comentarios de Inventario:</label>
+            <div class="col-md-9 col-sm-9 col-xs-12">  
+                    
+                    <textarea name="comentInventario" id="" cols="30" rows="4" class="form-control" placeholder="Partes adicionales que ingresaron o daños específicos"></textarea>
             </div><!--columna -->
         </div><!--form group -->
     </div> {{-- x-content --}}
